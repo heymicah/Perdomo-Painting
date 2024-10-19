9bcc77566a4a345398df04c4166e083d2fa52547
heymicah <micahtam70@gmail.com>
2024-10-19 00:23:35 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Perdomo's Painting</h1>
+        <p>
+          Perdomo's Painting is the best painting company in the world
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
